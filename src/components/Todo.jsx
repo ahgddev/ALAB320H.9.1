eef5e0ffd13562332b07a26a8987c55eb665c1d8
ahgddev <ahgddev@gmail.com>
2024-03-12 15:09:01 -0700

    Todos are programatically added to the ToDo List. - ToDos are automatically set to Complete or Not Complete on load. - ToDos can be marked as Complete or Not Complete.

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,22 +1,32 @@
-import {useState} from "react"
+import { useEffect, useState } from "react";
 
-function Todo({todoID, todoText, isComplete}) {
- const [checkedState, setChecked] = useState(false)
+function Todo({ todoID, todoText, isComplete }) {
+  const [checkedState, setChecked] = useState();
 
- function updateCheckedState(){
-  setChecked(this.target.checked)
- }
+  function updateCheckedState() {
+    setChecked(checked && !checked);
+  }
 
   return (
     <div className="todoItem">
       <p id="todoID">{todoID}</p>
       <p id="todoStr">{todoText}</p>
-      <input type="checkbox" name="completed" id="completeChkBox" onChange={updateCheckedState}/>
-      <label for="completed" if>Completed</label>
+      <input
+        type="checkbox"
+        name="completed"
+        id="completeChkBox"
+        defaultChecked={isComplete}
+        onChange={updateCheckedState}
+      />
+      <label for="completed" if>
+        Completed
+      </label>
       <button name="editBtn">Edit ToDo</button>
-      <button name="deleteBtn" disabled>Delete ToDo</button>
+      <button name="deleteBtn" disabled>
+        Delete ToDo
+      </button>
     </div>
-  )
+  );
 }
 
-export default Todo
+export default Todo;
