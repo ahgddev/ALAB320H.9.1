50733abf099ec3db2781ade928c2adc2961131ff
ahgddev <ahgddev@gmail.com>
2024-03-12 13:18:36 -0700

    Added checkbox component.

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,8 +2,10 @@ function Todo(props) {
   return (
     <div className="todoItem">
       <p id="todoStr">Todo Item</p>
-      <button name="deleteBtn">Delete ToDo</button>
+      <input type="checkbox" name="completed" id="completeChkBox" />
+      <label for="completed">Mark as Completed</label>
       <button name="editBtn">Edit ToDo</button>
+      <button name="deleteBtn" disabled>Delete ToDo</button>
     </div>
   )
 }
