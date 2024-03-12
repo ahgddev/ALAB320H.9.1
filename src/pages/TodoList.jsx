39ff0777e649a264830a323f6cda1483099246a5
ahgddev <ahgddev@gmail.com>
2024-03-12 14:55:13 -0700

    Grabbing todos works.

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,15 +1,23 @@
-import Todo from "../components/Todo"
+import Todo from "../components/Todo";
+import { initialState as data } from "../database/data";
 
 function TodoList() {
   return (
     <>
-    <h1>Todo List</h1>
-    <input type="text" id="addTodo" />
-    <Todo />
-    <Todo />
-    <Todo />
+      <h1>Todo List</h1>
+      <input type="text" id="addTodo" />
+      <div id="todoContainer">
+        {data.map((todoData) => (
+          <Todo
+            key={todoData.id}
+            todoID={todoData.id}
+            todoText={todoData.title}
+            isComplete={todoData.completed}
+          />
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default TodoList
+export default TodoList;
