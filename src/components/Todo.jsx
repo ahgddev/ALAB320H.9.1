c19206ce422d4f5fc7645831316be4d5b1ac66e9
ahgddev <ahgddev@gmail.com>
2024-03-12 14:28:12 -0700

    Fixed checkbox state not updating.

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,18 @@
-function Todo(props) {
+import {useState} from "react"
+
+function Todo({todoID, todoText, isComplete}) {
+ const [checkedState, setChecked] = useState(false)
+
+ function updateCheckedState(){
+  setChecked(this.target.checked)
+ }
+
   return (
     <div className="todoItem">
-      <p id="todoStr">Todo Item</p>
-      <input type="checkbox" name="completed" id="completeChkBox" />
-      <label for="completed">Mark as Completed</label>
+      <p id="todoID">{todoID}</p>
+      <p id="todoStr">{todoText}</p>
+      <input type="checkbox" name="completed" id="completeChkBox" onChange={updateCheckedState}/>
+      <label for="completed" if>Completed</label>
       <button name="editBtn">Edit ToDo</button>
       <button name="deleteBtn" disabled>Delete ToDo</button>
     </div>
