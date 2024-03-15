4a5e053a541419cf0eadd0b7c1a3554abef38595
ahgddev <ahgddev@gmail.com>
2024-03-15 11:58:15 -0700

    Successful conversion of Todo deletion to a reducer function.

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,18 +1,53 @@
-import {useState, useEffect} from "react"
+import {useState, useReducer} from "react"
 import Todo from "../components/Todo";
 import { initialState as data } from "../database/data";
 
+const ACTIONS = {
+  DELETE_TODO: "delete_todo",
+  ADD_TODO: "add_todo"
+  }
+
+  function reducer(state, action){
+    switch (action.type){
+      case "delete_todo":
+        return(
+          state.filter((todos) => todos.id !== action.payload.ID)
+        )
+    case ACTIONS.ADD_TODO:
+      return (
+        [newTodo(action.payload.title, action.payload.nextID), ...state]
+      )
+    default:
+      return state
+  }
+
+}
+
+function newTodo(title, nextID){
+return ({
+  "userId": 1,
+  "id": nextID + 1,
+  "title": title,
+  "completed": false
+})
+}
+
 function TodoList() {
-const [TodoData, setTodoData] = useState(data)
+const [TodoData, dispatch] = useReducer(reducer, data)
+// const [daata, dispatch] = useReducer(reducer, data)
+const [todoText, setTodoText] = useState('');
 
 function deleteTodo(ID){
-  setTodoData(TodoData.filter((todos) => todos.id !== ID))
+  setTodoData()
 }
 
   return (
     <>
       <h1>Todo List</h1>
-      <input type="text" id="addTodo" />
+      <input type="text" id="addTodo" onChange={(e) => setTodoText(e.target.value)}/>
+      <button id="addTodo" onClick={() => {
+          dispatch({ type: ACTIONS.ADD_TODO, payload: { title: todoText, nextID: TodoData.length} })
+        }}>Add ToDo</button>
       <div id="todoContainer">
         {TodoData.map((todoData) => (
           <Todo
@@ -20,7 +55,8 @@ function deleteTodo(ID){
             todoID={todoData.id}
             todoText={todoData.title}
             isComplete={todoData.completed}
-            deleteFunc={deleteTodo}
+            deleteFunc={() => {dispatch({ type: ACTIONS.DELETE_TODO, payload: { ID: todoData.id }})
+          }}
           />
         ))}
        
